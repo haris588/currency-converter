1ed3097dead7f50cba5cd91d60ebd6a8205055a5
haris588 <hharis558@gmail.com>
2021-03-19 18:55:07 +0200

    Add a reset function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const currencyFromInput = document.getElementById('currencyFrom')
 const currencyToInput = document.getElementById('currencyTo')
-const currencyAmountFromInput = document.getElementById('currencyAmountInputFrom')
+const currencyAmountInput = document.getElementById('currencyAmountInputFrom')
 const btnConvert = document.getElementById('btnConvert')
 const resultContainer = document.querySelector('.resultContainer')
 
 let currencyFrom,
     currencyTo,
-    currencyAmountFrom
+    currencyAmount
 
 const exchange = async () => {
     const api = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyFrom}` //<---- currency FROM
@@ -16,12 +16,24 @@ const exchange = async () => {
     const { conversion_rates } = apiData
     // currency FROM * coversion_rates.TO
 
-    const convert = currencyAmountFrom * conversion_rates[currencyTo]
+    const convert = currencyAmount * conversion_rates[currencyTo]
 
     const p = document.createElement('p')
     p.classList.add('convertedAmount')
-    p.innerHTML = `${currencyAmountFrom} ${currencyFrom} = ${convert.toFixed(2)} ${currencyTo}`
+    p.innerHTML = `${currencyAmount} ${currencyFrom} = ${convert.toFixed(2)} ${currencyTo}`
+
+    reset()
+
     return resultContainer.appendChild(p)
+}
+
+const reset = () => {
+    currencyFromInput.value = ""
+    currencyToInput.value = ""
+    currencyAmountInput.value = ""
+    currencyFrom = ""
+    currencyTo = ""
+    currencyAmount = ""
 }
 
 currencyFromInput.addEventListener('change', (e) => {
@@ -32,8 +44,8 @@ currencyToInput.addEventListener('change', (e) => {
     currencyTo = e.target.value.toUpperCase()
 })
 
-currencyAmountFromInput.addEventListener('change', (e) => {
-    currencyAmountFrom = e.target.value
+currencyAmountInput.addEventListener('change', (e) => {
+    currencyAmount = e.target.value
 })
 
 btnConvert.addEventListener('click', exchange)
